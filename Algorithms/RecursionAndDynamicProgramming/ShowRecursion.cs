73e11c59a88e0419bba5ec657bec7b44b60f4e18
Jack.Hsu <jackhsu0225@gmail.com>
2021-11-10 12:44:05 -0500

    Add result at the end to ShowRecursion

--- Algorithms/RecursionAndDynamicProgramming/ShowRecursion.cs
+++ Algorithms/RecursionAndDynamicProgramming/ShowRecursion.cs
@@ -8,17 +8,23 @@ namespace Algorithms.RecursionAndDynamicProgramming
     {
         public void Execute(int n)
         {
-            Execute(n, n);
+            Execute(n, n, new char[n + 1], 0);
         }
 
-        private void Execute(int total, int n)
+        private void Execute(int total, int n, char[] str, int index)
         {
             if (n < 0)
+            {
+                Console.WriteLine(GetTabs(total) + new string(str));
                 return;
+            }
+
+            str[index] = '1';
             Console.WriteLine(GetTabs(total - n) + "n: " + n + " 1");
-            Execute(total, n - 1);
+            Execute(total, n - 1, str, index + 1);
+            str[index] = '2';
             Console.WriteLine(GetTabs(total - n) + "n: " + n + " 2");
-            Execute(total, n - 1);
+            Execute(total, n - 1, str, index + 1);
         }
 
         private string GetTabs(int number)
